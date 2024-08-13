750f3ff24dca56c8cb852ee4241bbedeb7d3b152
Bar Volovski <bar.volovski@post.runi.ac.il>
2024-08-13 17:02:28 +0300

    test

--- books-service/tests/assn3_tests.py
+++ books-service/tests/assn3_tests.py
@@ -1,16 +1,64 @@
 import requests
 
-def test_create_book():
-    url = "http://localhost:5001/books"
-    payload = {
-        "title": "Adventures of Huckleberry Finn",
-        "ISBN": "9780520343641",
-        "genre": "Fiction"
-    }
-    response = requests.post(url, json=payload)
-    assert response.status_code == 201
-    data = response.json()
-    assert "ID" in data
-    assert len(data["ID"]) > 0
+BASE_URL = "http://localhost:5001/books"
 
-# Add more tests based on the assignment requirements
+book6 = {
+    "title": "The Adventures of Tom Sawyer",
+    "ISBN": "9780195810400",
+    "genre": "Fiction"
+}
+
+book7 = {
+    "title": "I, Robot",
+    "ISBN": "9780553294385",
+    "genre": "Science Fiction"
+}
+
+book8 = {
+    "title": "Second Foundation",
+    "ISBN": "9780553293364",
+    "genre": "Science Fiction"
+}
+
+books_data = []
+
+
+def test_post_books():
+    books = [book6, book7, book8]
+    for book in books:
+        res = requests.post(BASE_URL, json=book)
+        assert res.status_code == 201
+        res_data = res.json()
+        assert "ID" in res_data
+        books_data.append(res_data)
+        books_data_tuples = [frozenset(book.items()) for book in books_data]
+    assert len(set(books_data_tuples)) == 3
+
+
+def test_get_query():
+    res = requests.get(f"{BASE_URL}?authors=Isaac Asimov")
+    assert res.status_code == 200
+    assert len(res.json()) == 2
+
+
+def test_delete_book():
+    res = requests.delete(f"{BASE_URL}/{books_data[0]['ID']}")
+    assert res.status_code == 200
+
+
+def test_post_book():
+    book = {
+        "title": "The Art of Loving",
+        "ISBN": "9780062138927",
+        "genre": "Science"
+    }
+    res = requests.post(BASE_URL, json=book)
+    assert res.status_code == 201
+
+
+
+def test_get_new_book_query():
+    res = requests.get(f"{BASE_URL}?genre=Science")
+    assert res.status_code == 200
+    res_data = res.json()
+    assert res_data[0]["title"] == "The Art of Loving"
